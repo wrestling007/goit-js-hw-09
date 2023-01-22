b3b5229ec772c6c807813f9530470cb0423512a5
wrestling007 <bagetta007@gmail.com>
2023-01-22 18:51:48 +0200

    refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector('input[name-delay]'),
+  step: document.querySelector('input[name-step]'),
+  amount: document.querySelector('input[name-amount]'),
+  btnSubmit: document.querySelector('button'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {

5d995bc4bf63d3047608eebbda6f85766ae65fe4
Viacheslav ALEKSIEIEV <bagetta007@gmail.com>
2023-01-22 12:58:55 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+function setBodyColor() {
+  refs.body.style.background = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let changeId = null;
+
+refs.startBtn.addEventListener("click", () => {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+   changeId = setInterval(setBodyColor, 1000) 
+})
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(changeId);
+  refs.stopBtn.removeAttribute('disabled');  
+})
 
